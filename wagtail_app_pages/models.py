8512f8d7628cd680c0c12ce0246b9cc4c65b0f33
Marco Westerhof <m.westerhof@lukkien.com>
2018-07-20 18:32:41 +0200

    WIP: previews

--- wagtail_app_pages/models.py
+++ wagtail_app_pages/models.py
@@ -8,9 +8,32 @@ class AppPageMixin:
     def url_config(self):
         raise NotImplementedError('url_config')
 
+    @property
+    def is_preview_instance(self):
+        served_through_preview = getattr(self, '_served_through_preview', False)
+        return served_through_preview
+
+    @staticmethod
+    def _get_url_components(url):
+        url_parts = url.split('?', 1)
+        if len(url_parts) == 1:
+            return url, None
+        return url_parts
+
     def reverse(self, name, *args, **kwargs):
         sub_url = self._apppage_url_resolver.reverse(name, *args, **kwargs)
-        return self.url + sub_url.lstrip('/')
+        url = self.url + sub_url.lstrip('/')
+        url, query_string = self._get_url_components(url)
+
+        if self.is_preview_instance:
+            if query_string:
+                query_string += '&preview'
+            else:
+                query_string = 'preview'
+
+        if query_string:
+            return '?'.join([url, query_string])
+        return url
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -33,13 +56,19 @@ class AppPageMixin:
         if view is None:
             return super().serve(request, *args, **kwargs)
 
-        request.parent_page = self
+        page_instance = self
+        if request.GET.get('preview') is not None:
+            page_instance = page_instance.get_latest_revision_as_page()
+
+        request.parent_page = page_instance
+
         # if this is a class-based view, we'll make the parent page available as an attribute as well
         if getattr(view, 'view_class', None):
-            view.view_class.parent_page = self
+            view.view_class.parent_page = page_instance
 
         return view(request, *args, **kwargs)
 
     def serve_preview(self, request, mode_name):
+        self._served_through_preview = True
         result = self.route(request, [])
         return self.serve(request, *result.args)
