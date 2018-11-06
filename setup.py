fffebf2907fb0bac1c938fe1ec594839457a78cb
Marco Westerhof <m.westerhof@lukkien.com>
2018-11-06 10:56:48 +0100

    update changelog and version number

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite='tests.testproject.testproject.tests',
     tests_require=test_requirements,
     url='https://github.com/mwesterhof/wagtail_app_pages',
-    version='0.2.5',
+    version='0.2.6',
     zip_safe=False,
 )
