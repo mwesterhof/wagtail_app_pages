22751d5bae71b1616e835e77c49a0e1a6dfb3bdd
Marco Westerhof <m.westerhof@lukkien.com>
2020-06-17 13:41:49 +0200

    update 0.2.15

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite='tests.testproject.testproject.tests',
     tests_require=test_requirements,
     url='https://github.com/mwesterhof/wagtail_app_pages',
-    version='0.2.14',
+    version='0.2.15',
     zip_safe=False,
 )
