723a659c9d9ba1d6680a131e1438fa00d5d42bfe
Rebecca Claire Murphy <hi@rcm.dev>
2021-05-22 21:45:59 -0400

    Add Wagtail 2.12 and 2.13 support

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@
 
 from setuptools import setup, find_packages
 
-with open('README.md') as readme_file:
+with open("README.md") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
 requirements = [
-    'wagtail>=1.11.1,<2.12',
+    "wagtail>=1.11.1,<2.14",
 ]
 
 setup_requirements = []
@@ -21,31 +21,31 @@ test_requirements = []
 
 setup(
     author="Marco Westerhof",
-    author_email='westerhof.marco@gmail.com',
+    author_email="westerhof.marco@gmail.com",
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
         "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
     description="Full MVC support for wagtail pages",
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='wagtail_app_pages',
-    name='wagtail_app_pages',
-    packages=find_packages(include=['wagtail_app_pages']),
+    keywords="wagtail_app_pages",
+    name="wagtail_app_pages",
+    packages=find_packages(include=["wagtail_app_pages"]),
     setup_requires=setup_requirements,
-    test_suite='tests.testproject.testproject.tests',
+    test_suite="tests.testproject.testproject.tests",
     tests_require=test_requirements,
-    url='https://github.com/mwesterhof/wagtail_app_pages',
-    version='0.2.17',
+    url="https://github.com/mwesterhof/wagtail_app_pages",
+    version="0.2.17",
     zip_safe=False,
 )
