78315149fb09a1b6cbd3a1a15717c93cdadd02ce
Marco Westerhof <m.westerhof@lukkien.com>
2021-07-21 16:13:07 +0200

    preparing next release

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite="tests.testproject.testproject.tests",
     tests_require=test_requirements,
     url="https://github.com/mwesterhof/wagtail_app_pages",
-    version="0.2.17",
+    version="0.2.19",
     zip_safe=False,
 )
--- wagtail_app_pages/__init__.py
+++ wagtail_app_pages/__init__.py
@@ -4,4 +4,4 @@
 
 __author__ = """Marco Westerhof"""
 __email__ = 'westerhof.marco@gmail.com'
-__version__ = '0.2.17'
+__version__ = '0.2.19'
